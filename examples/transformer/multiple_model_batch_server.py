24e6a92dd3bd0afeda74a160f8bda032031bcd01
Daniel Lim <dalim@seek.com.au>
2024-01-26 21:37:49 +1100

    Add different server configuration.

---
+++ examples/transformer/multiple_model_batch_server.py
@@ -0,0 +1,52 @@
+
+import logging
+import numpy as np
+import itertools
+import time
+
+from sentence_transformers import SentenceTransformer
+
+from pytriton.decorators import batch
+from pytriton.model_config import ModelConfig, Tensor
+from pytriton.triton import Triton
+
+logger = logging.getLogger("performance-testing-smallBert")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s: %(message)s")
+
+sbertmodel = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+@batch
+def _infer_fn(**inputs: np.ndarray):
+    ts = time.time()
+    (sequence_batch,) = inputs.values()
+
+    sequence_batch: np.ndarray = np.char.decode(sequence_batch.astype("bytes"), "utf-8")
+    input_texts = [text for batch in sequence_batch for text in batch]
+    print(f"Decoding  : {int((time.time() - ts) * 1000)}ms")
+    ts = time.time()
+    vector = sbertmodel.encode(sentences=input_texts)
+    print(f"Model runtime  : {int((time.time() - ts) * 1000)}ms")
+
+    return [vector]
+
+
+with Triton() as triton:
+    logger.info("Loading smallBert model.")
+    triton.bind(
+        model_name="smallBert",
+        infer_func=[_infer_fn, _infer_fn],
+        inputs=[
+            Tensor(name="text", dtype=np.bytes_, shape=(1,)),
+        ],
+        outputs=[
+            Tensor(
+                name="response",
+                dtype=np.float32,
+                shape=(-1,),
+            ),
+        ],
+        config=ModelConfig(max_batch_size=50),
+        strict=True,
+    )
+    logger.info("Serving inference")
+    triton.serve()
--- readme.md
+++ readme.md
@@ -10,7 +10,12 @@ Start container in interactive mode.
 docker run -it -p8000:8000 -p8001:8001 -p8002:8002 -v ./non-existent-path:/models -v ./pytriton_examples:/examples pytriton-load-test-example-transformer bash
 ```
 
+```sh
+docker run --gpus=1 -it -p8000:8000 -p8001:8001 -p8002:8002 -v ./non-existent-path:/models -v ./pytriton_examples:/examples pytriton-load-test-example-transformer bash
+```
+
+
 Run command within the container.
 ```sh
-./transformer/server.py
+python3 ./transformer/multiple_model_single_record_server.py
 ```
